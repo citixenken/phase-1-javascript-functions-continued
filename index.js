661156ddf98397d9694cea7a0b86cf0d7a475003
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-07 22:43:00 +0300

    commit tree ec91e2d4b74591d3ee1ef0618d04fb5a295c8a67

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+
+//1. saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+// console.log(saturdayFun("code"));
+// console.log(saturdayFun());
+
+//2. mondayWork function
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+// console.log(mondayWork("code"));
+// console.log(mondayWork());
+
+//3. wrapAdjective function
+function wrapAdjective(ending = "*") {
+  const innerFn = function (occassion = "special") {
+    return `You are ${ending}${occassion}${ending}!`;
+  };
+  //   return innerFn("beautiful");
+  return innerFn;
+}
+// console.log(wrapAdjective("%")("a dedicated programmer"));
